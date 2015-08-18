d6023fab753001de6eed3a82e67f77f7996f3fee
최영환[네무소프트] <ne90418@nhnent.com>
2015-08-18 10:55:21 +0900

    angular-busy-master 추가, require추가

--- src/main/webapp/static/js/require/app.js
+++ src/main/webapp/static/js/require/app.js
@@ -3,14 +3,14 @@
 // Configure loading modules from the lib directory,
 // except for 'app' ones, which are in a sibling
 // directory.
-requirejs.config({
-    baseUrl: '/static/js/lib/require',
-    paths: {
-        app: '/static/js/require',
-        common: '/static/js/common'
-    }
-});
+//requirejs.config({
+//    baseUrl: '/static/js/lib/require'
+//    paths: {
+//        app: '/static/js/require',
+//        common: '/static/js/common'
+//    }
+//});
 
 // Start loading the main app file. Put all of
 // your application logic in there.
-requirejs(['app/main']);
+//requirejs(['app/main']);
